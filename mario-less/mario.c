19311109e1d5e2b31719e33c6f448631ef17e591
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2022-11-20 04:19:33 +0000

    Sun, Nov 20, 2022, 4:19 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,9 +1,10 @@
+
 #include <cs50.h>
 #include <stdio.h>
+
 int main(void)
 {
-    int i = get_int("Input: );
-    printf("Output: %i\n",i);
-
+    int i = get_int("Input: ");
+    printf("Output: %i\n", i);
 }
 SEE ALSO
