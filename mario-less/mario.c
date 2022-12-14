eb5bd301442ca9607c643740120df354d79152b4
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2022-12-14 17:09:11 +0000

    Wed, Dec 14, 2022, 5:09 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,23 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <cs50.h>
-int main()
-{
-    int height = 0;
-    printf("Please enter an integer.\n");
-    scanf("%d", &height);
-    while (height < 1 || height > 8) {
-        printf("Please enter an integer.\n");
-        scanf("%d", &height);
-    } printf("The height is %d.\n", height);
-    for (int h = 1; h <= height; h++) {
-        for (int j = 0; j <= height - h; j++) {
-            printf(" ");
-        } for (int k = 0; k <= h - 1; k++) {
-                printf("#");
-            }
-            printf("\n");
-    }
+#include <stdio.h>
 
-    return 0;
+int main(void) 
+{
+    
 }
