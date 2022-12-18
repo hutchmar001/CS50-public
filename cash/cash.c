ef6a31456798c2e2903ad2f1a6edac0ccb9cb60b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2022-12-18 03:57:38 +0000

    Sun, Dec 18, 2022, 3:57 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -43,6 +43,7 @@ int get_cents(void){
 
     while (cents <= 0)
     {
+        printf("Please enter the amount of cents. ");
         int c;
         while((c=getchar())!='\n' && c!=EOF);
         scanf("%d", &cents);
