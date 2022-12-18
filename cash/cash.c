e7ff07cc2d8cc17a8491842bca2b7be4781203b0
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2022-12-18 01:12:46 +0000

    Sun, Dec 18, 2022, 1:12 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,13 +1,11 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int get_cents(void);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
-
-int numCents;
 
 int main(void)
 {
@@ -37,37 +35,43 @@ int main(void)
     printf("%i\n", coins);
 }
 
-int get_cents(void)
-{
-    do {
-
-        numCents = get_int("Input: ");
-        printf("%d", numCents);
+int get_cents(void){
+    printf("Please enter the amount of cents. ");
+    int cents;
+    scanf("%d", &cents);
+    while (cents < 1)
+    {
+        printf("Please enter the amount of cents. ");
+        scanf("%d", &cents);
     }
-    while (numCents > 0);
+    return cents;
 }
+
 
 int calculate_quarters(int cents)
 {
-    // TODO
-    return 0;
+    int q = (int)floor(cents/25);
+    printf("%d\n", q);
+    return q;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
-    return 0;
+    int d = (int)floor(cents/10);
+    printf("%d\n", d);
+    return d;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    int n = (int)floor(cents/5);
+    printf("%d\n", n);
+    return n;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    int p = (int)floor(cents);
+    printf("%d\n", p);
+    return p;
 }
-
