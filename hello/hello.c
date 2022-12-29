de634a47f5279557ba6570a52332d8f4c7f96f47
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2022-12-29 02:42:53 +0000

    Thu, Dec 29, 2022, 2:42 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdin.h>
 
 int main(void)
 {
     printf("hello, world\n");
-    string str= get_string("What's your name? ");
-    printf("hello, %s\n", str);
+    string name= get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
