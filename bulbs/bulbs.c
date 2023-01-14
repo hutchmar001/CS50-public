e1804aee21c4e5ceaa9855cc9ef39496e10d305b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-01-14 22:17:01 +0000

    Sat, Jan 14, 2023, 10:17 PM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -12,7 +12,7 @@ int convert2_num(string word)
     for (i = 0; i < (strlen(word)); i++)
     {
         char str[] = {word[i]}; // Makes character into string
-        int  = strtol(str, NULL, 36) - 10; //Converts letter to number using base 36
+        int num = strtol(str, NULL, 36) - 10; //Converts letter to number using base 36
     };
     return num;
 }
