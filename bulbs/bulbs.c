20cf21c355a1d61d977d430455c640720662d687
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-01-16 02:37:19 +0000

    Mon, Jan 16, 2023, 2:37 AM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -7,7 +7,7 @@ const int BITS_IN_BYTE = 8;
 
 void print_bulb(int bit);
 
-int convert2_num(word);
+int convert2_num(string word);
 {
     int i;
     int num;
@@ -15,7 +15,7 @@ int convert2_num(word);
     {
         char str[] = {word[i]}; // Makes character into string
         num = strtol(str, NULL, 36) + 55; //Converts letter to number using base 10
-    s1.dec[i] = num;
+    dec[i] = num;
     };
     return dec;
 }
