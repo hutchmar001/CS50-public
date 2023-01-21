953e678556115b57c9435df07d843774eb1608d2
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-01-21 02:05:47 +0000

    Sat, Jan 21, 2023, 2:05 AM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -28,9 +28,11 @@ int main(void)
             array[j] /= 2;
             binary[k] = 0;
             k++;
+            continue;
         } else {
             binary[k] = 1;
             k++;
+            continue;
         }
 };
 
