49efc5da8f532c4fdec00cc6a03007688f0f75e3
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-06 03:03:16 +0000

    Sun, Feb 5, 2023, 9:03 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,10 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
-
+int count_letters(string text);
 int main(void)
 {
-    int count_letters(string text);
     string txt = get_string("Text: ");
     printf("%s\n", txt);
     count_letters(txt);
