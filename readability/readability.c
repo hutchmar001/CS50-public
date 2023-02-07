4aac98b953138cd1c4cf5f279a0338db93ab1874
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-07 04:03:11 +0000

    Mon, Feb 6, 2023, 10:03 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,10 +3,12 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text){
+int count_letters(string text)
+{
     int count = 0;
     int i;
-    for (i = 0; i < strlen(text); i++) {
+    for (i = 0; i < strlen(text); i++)
+    {
         char txt[1];
         txt[0] = text[i];
         char blank[1];
@@ -17,38 +19,46 @@ int count_letters(string text){
         expoint[0] = '!';
         char questionmark[1];
         questionmark[0] = '?';
+        char apostrophe[1];
+        apostrophe[0] = ''';
         int result1 = memcmp(txt, blank, 1);
         int result2 = memcmp(txt, period, 1);
         int result3 = memcmp(txt, expoint, 1);
         int result4 = memcmp(txt, questionmark, 1);
-        if (result1 != 0 && result2 != 0 && result3 != 0 && result4 != 0) {
+        int result5 = memcmp(txt, apostrophe, 1);
+        if (result1 != 0 && result2 != 0 && result3 != 0 && result4 != 0 && result5 != 0)
+        {
             count++;
         }
-                }
+    }
     return count;
 }
 
-int count_words(string text){
+int count_words(string text)
+{
     int count2 = 1;
     int j;
-    for (j = 0; j < strlen(text); j++) {
+    for (j = 0; j < strlen(text); j++)
+    {
         char txt[1];
         txt[0] = text[j];
         char blank[1];
         blank[0] = ' ';
         int result = memcmp(txt, blank, 1);
-        if (result == 0) {
-            count2++; //Counting amount of spaces w/ 1 added, space returns neg.
+        if (result == 0)
+        {
+            count2++; // Counting amount of spaces w/ 1 added, space returns neg.
         }
-                }
+    }
     return count2;
-
 }
 
-int count_sentences(string text){
+int count_sentences(string text)
+{
     int count3 = 0;
     int k;
-    for (k = 0; k < strlen(text); k++) {
+    for (k = 0; k < strlen(text); k++)
+    {
         char txt[1];
         txt[0] = text[k];
         char period[1];
@@ -60,10 +70,11 @@ int count_sentences(string text){
         int result1 = memcmp(txt, period, 1);
         int result2 = memcmp(txt, expoint, 1);
         int result3 = memcmp(txt, questionmark, 1);
-        if (result1 == 0 || result2 == 0 || result3 == 0) {
+        if (result1 == 0 || result2 == 0 || result3 == 0)
+        {
             count3++;
         }
-                }
+    }
     return count3;
 }
 
