9057fe9671c53c11db22b4c97ddddb294d5a7415
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-07 03:56:32 +0000

    Mon, Feb 6, 2023, 9:56 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -21,7 +21,7 @@ int count_letters(string text){
         int result2 = memcmp(txt, expoint, 1);
         int result3 = memcmp(txt, questionmark, 1);
         if (result1 == 0 || result2 == 0 || result3 == 0) {
-            count3++;
+            count++;
         }
                 }
     return count;
