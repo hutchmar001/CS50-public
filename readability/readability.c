f120418e7779fcfce91a70c6115da5a62238bd56
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-07 00:22:18 +0000

    Mon, Feb 6, 2023, 6:22 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int count_letters(string text){
     int count = 0;
     int i;
-    for (i = 0; i < len(text); i++) {
+    for (i = 0; i < strlen(text); i++) {
         if(text[i] != " ")
             {
                 count += 1;
