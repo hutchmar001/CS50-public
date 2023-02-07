ae388aed07f380a8777c5bc6a8354ab27b1a7e42
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-07 03:13:40 +0000

    Mon, Feb 6, 2023, 9:13 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -50,8 +50,8 @@ int count_sentences(string text){
         int result1 = memcmp(txt, blank, 1);
         int result2 = memcmp(txt, expoint, 1);
         int result3 = memcmp(txt, questionmark, 1); //Switch to memcmp to compare strings
-        printf("%d\n", result);
-        if (result1 == 0) {
+        printf("%d\n", result1);
+        if (result1 == 0 ) {
             count3++;
         }
                 }
