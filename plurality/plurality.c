d8afd808521f37b3153c44547842bce16124ed1c
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-02-27 18:41:54 +0000

    Mon, Feb 27, 2023, 12:41 PM -06:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -64,6 +64,7 @@ int main(int argc, string argv[])
     for (int j = 0; j < candidate_count; j++)
     {
         arr[j] = candidates[j].votes;
+        return arr[];
     }
 
     void most_votes(int arr[]);
