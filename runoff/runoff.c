d72734db29d22a3b0aeb71528796c9a23a4daf68
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-02 03:38:17 +0000

    Wed, Mar 1, 2023, 9:38 PM -06:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,11 +141,12 @@ bool vote(int voter, int rank, string name)
             }
             else
             {
-            return false;
+            continue;
             }
             }
 
 }
+return 0;
 }
 
 // Tabulate votes for non-eliminated candidates
