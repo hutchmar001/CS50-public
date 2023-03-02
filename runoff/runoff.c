9d92eea9fb060a20516226fa8f8ef6e9b30dd983
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-02 02:02:53 +0000

    Wed, Mar 1, 2023, 8:02 PM -06:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
     int result = strcmp(name, candidates[k].name);
     if (result == 0)
     {
-    preferences[j][k] = strcopy(name
+    memcpy(preferences[j][k],"name",sizeof("name"));
     }
     else
     {
