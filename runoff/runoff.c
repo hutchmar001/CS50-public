79a0379de6c7c85fe46fc9a489b13c9095554a7f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-03 20:36:59 +0000

    Fri, Mar 3, 2023, 2:36 PM -06:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,25 +128,19 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int j = 0; j < voter_count; j++)
-    {
-        for (int k = 0; k < candidate_count; k++)
-            {
-            int result = strcmp(name, candidates[k].name);
+
+            int result = strcmp(name, candidates[rank].name);
             printf("%d\n", result);
             if (result == 0)
             {
-            preferences[j][k] = k;
-            printf("%d\n", preferences[j][k]);
+            preferences[voter][rank] = rank;
+            printf("%d\n", preferences[voter][rank]);
             return true;
             }
             else
             {
-            continue;
+            return false;
             }
-            }
-}
-return false;
 }
 
 // Tabulate votes for non-eliminated candidates
