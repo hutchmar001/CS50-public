fac5c98a5111c8c353f2706857297cfe93828dd5
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-03 18:11:10 +0000

    Fri, Mar 3, 2023, 12:11 PM -06:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -136,7 +136,7 @@ bool vote(int voter, int rank, string name)
             printf("%d\n", result);
             if (result == 0)
             {
-            preferences[j][k] = preferences[voter][rank];
+            preferences[j][k] = candidates[k];
             printf("%d\n", preferences[j][k]);
             return true;
             }
