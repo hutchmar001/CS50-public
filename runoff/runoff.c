3ab4b4213fb6ec721c490551c8de03159976417d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-04 00:32:52 +0000

    Fri, Mar 3, 2023, 6:32 PM -06:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,11 +145,12 @@ bool vote(int voter, int rank, string name)
             continue;
             }
             }
+
+}
             printf("%d\n", preferences[0][0]);
             printf("%d\n", preferences[0][1]);
             printf("%d\n", preferences[1][0]);
             printf("%d\n", preferences[1][1]);
-}
 return false;
 }
 
