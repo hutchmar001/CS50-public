1e07d6ddead050feb83b9977828218f308c703cc
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-21 18:10:46 +0000

    Tue, Mar 21, 2023, 12:10 PM -06:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,8 +6,8 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     pixel.rgbtBlue = 50;
     pixel.rgbtGreen = 50;
     pixel.rgbtRed = 50;
-    for (int h = 0; h < 20; h++)
-        for (int w = 0; w < 20; w++)
+    for (int h = 0; h < 5; h++)
+        for (int w = 0; w < 5; w++)
             image[h][w] = image[0][0];
     // Change all black pixels to a color of your choosing
 }
