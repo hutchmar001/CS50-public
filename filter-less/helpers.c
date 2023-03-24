d0150d887610821c6db722a3036dad1a9f61eb13
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-24 14:22:59 +0000

    Fri, Mar 24, 2023, 3:22 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -134,7 +134,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                                             + image[h - 1][w].rgbtBlue + image[h - 1][w - 1].rgbtBlue
                                             + image[h][w - 1].rgbtBlue + image[h + 1][w - 1].rgbtBlue) / 6) -10;
             }
-            else if (h == height - 1 && 0 < w < width - 1)
+            else if (h == height - 1 && 0 < w && < width - 1)
             {
                 copy[h][w].rgbtRed = (round(image[h][w].rgbtRed + image[h][w + 1].rgbtRed
                                            + image[h - 1][w + 1].rgbtRed + image[h - 1][w].rgbtRed
@@ -146,7 +146,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                                             + image[h - 1][w + 1].rgbtBlue + image[h - 1][w].rgbtBlue
                                             + image[h - 1][w - 1].rgbtBlue + image[h][w - 1].rgbtBlue) / 6) -10;
             }
-            else if (0 < h < height - 1 && w == 0)
+            else if (0 < h && h < height - 1 && w == 0)
             {
                 copy[h][w].rgbtRed = (round(image[h][w].rgbtRed + image[h + 1][w].rgbtRed
                                            + image[h + 1][w + 1].rgbtRed + image[h][w + 1].rgbtRed
