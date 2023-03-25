20c28cac963f78e952f4002b4017b9d19039c857
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-25 10:04:21 +0000

    Sat, Mar 25, 2023, 11:04 AM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -173,12 +173,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         image[h][w].rgbtRed = copy[h][w].rgbtRed;
         image[h][w].rgbtGreen = copy[h][w].rgbtGreen;
         image[h][w].rgbtBlue = copy[h][w].rgbtBlue;
-        printf("%i\n", copy[h][w].rgbtRed);
-        printf("%i\n", copy[h][w].rgbtGreen);
-        printf("%i\n", copy[h][w].rgbtBlue);
-        printf("%i\n", image[h][w].rgbtRed);
-        printf("%i\n", image[h][w].rgbtGreen);
-        printf("%i\n", image[h][w].rgbtBlue);
         }
     return;
 }
