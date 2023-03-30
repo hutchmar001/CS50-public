7bc4100a91ba1c8bf2c98a3741ede08cc04089b9
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-03-30 13:15:26 +0000

    Thu, Mar 30, 2023, 3:15 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
     char buffer[5000];
+    fread(buffer, sizeof(char), 5000, file);
     while (fread(&buffer, sizeof(char), 5000, file) == 5000)
 {
     if (buffer[0] == 255)
