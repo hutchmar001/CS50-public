1f30303965cea50ad56d87eefd5ba30d8493da70
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 17:58:04 +0000

    Mon, Apr 3, 2023, 7:58 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 }
 for (int i = 0; i < 5000; i++)
 {
-    if (strcmp(&buffer[i], "d9") == 0)
+    if (strcmp(buffer[i], "d9") == 0)
     {
     printf("%x\n", buffer[i]);
     }
