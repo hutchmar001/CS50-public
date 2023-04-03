57f5552157f454708efef3d230a4585d7f2f5f00
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 19:51:58 +0000

    Mon, Apr 3, 2023, 9:51 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,14 +8,15 @@ int main(int argc, char *argv[])
     int i = 0;
     unsigned char buffer[10000];
 
-    while (fread(buffer, 1, 512, file) == 512)
+    while (fread(buffer, sizeof(char), 512, file) == 512)
 {
     if (buffer[i] == 0xFF)
     {
-        fread(buffer, 1, 512, file);
+        fread(buffer, sizeof(char), 512, file);
     }
 }
-for (i = 0; i < 10; i++)
+for (i = 0; i < 10000; i++)
+    if (buffer[i] == 0xFF)
     {
         printf("%c\n", buffer[i]);
     }
