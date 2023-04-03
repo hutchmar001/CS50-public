7035a5188968ad415917027045d07865a3f6313e
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 20:13:05 +0000

    Mon, Apr 3, 2023, 10:13 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,13 +10,12 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, 1, file) == 1)
 {
-    if (buffer[0] == 255)
+    if (buffer[0] == 255 && buffer[1] == 0xd8 && buffer[2] == 0xff)
     {
         fread(buffer, 1, 1, file);
     }
 }
-for (i = 0; i < 10000; i++)
-    if (buffer[i] == 0xFF)
+for (i = 0; i < 10; i++)
     {
         printf("%d\n%d\n", buffer[i], buffer[i+1]);
     }
