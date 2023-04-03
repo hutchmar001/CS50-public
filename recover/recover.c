bc52f591e1276dc321ab20ab4c54bdd669ea2f8b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 18:53:51 +0000

    Mon, Apr 3, 2023, 8:53 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 }
 for (int i = 0; i < 10000; i++)
 {
-    if (buffer[i] == 216)
+    if (&buffer[i] == 216)
     {
     printf("%d\n%d\n%d\n", buffer[i], buffer[i+1], buffer[i+2]);
     }
