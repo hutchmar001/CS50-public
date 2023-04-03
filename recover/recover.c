c4c0e0d2ea76771c6c4dd1b2f8837ca60b55d3b3
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 18:11:50 +0000

    Mon, Apr 3, 2023, 8:11 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,16 +5,15 @@
 int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
-    const char buffer[5000];
+    unsigned char buffer[5000];
     while (fread(buffer, sizeof(char), 5000, file) == 5000)
 {
         fread(buffer, sizeof(char), 5000, file);
 }
 for (int i = 0; i < 5; i++)
 {
-    if (strcmp(buffer[i], "d9") == 0)
-    {
-    printf("%s\n", buffer[i]);
-    }
+    
+    printf("%d\n", buffer[i]);
+
 }
 }
