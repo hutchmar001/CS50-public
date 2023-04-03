a62bc4f8c20fa727fc6e1aad47d941da709d3399
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 20:37:51 +0000

    Mon, Apr 3, 2023, 10:37 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 512, 1, file) == 1)
 {
     fread(buffer, 512, 1, file);
-    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
 
     }
