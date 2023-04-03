1178c8c45995d184045fc729456ec7b3eea3c826
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-03 19:32:52 +0000

    Mon, Apr 3, 2023, 9:32 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
-    int    *buffer;
-    buffer = (int *)malloc(sizeof(int)*50);
+    int *buffer;
+    buffer = (int *)malloc(sizeof(int)*512);
 
     while (fread(buffer, 1, 512, file) == 512)
 {
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 }
 for (int i = 0; i < 10000; i++)
 {
-    if (buffer[i] == 0xFF && buffer[i+1] == 0xD8 && buffer[i+2] == 0xFF)
+    if (buffer[i] == 0xFF)
     {
     printf("%d\n%d\n%d\n", buffer[i], buffer[i+1], buffer[i+2]);
     }
