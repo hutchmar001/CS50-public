6b712403099912487ec78eeae49b0246c615a3d2
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-05 09:42:47 +0000

    Wed, Apr 5, 2023, 11:42 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,9 +18,10 @@ int main(int argc, char *argv[])
     fread(buffer, 512, 1, file);
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-        sprintf(jpegs, "%03i.jpg", i);
-        i++;
+        sprintf(jpegs, "%03i.jpg\n", i);
         printf("%c", jpegs[i]);
+        i++;
+
     }
 }
 }
