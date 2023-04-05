b73e18c70a3df77a17e300b1e47abb6e23d12cb2
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-05 09:26:36 +0000

    Wed, Apr 5, 2023, 11:26 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,7 +5,11 @@
 int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
-    int i = 0;
+    if (file == NULL)
+    {
+        printf("File cannot be opened for reading.");
+        return 1;
+    }
     unsigned char buffer[10000];
 
     while (fread(buffer, 512, 1, file) == 1)
@@ -14,8 +18,7 @@ int main(int argc, char *argv[])
     fread(buffer, 512, 1, file);
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-        
+
     }
-    i++;
 }
 }
