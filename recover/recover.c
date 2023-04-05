6174060cb697e436f3571c6dcba9998ea2c00943
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-05 09:45:52 +0000

    Wed, Apr 5, 2023, 11:45 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,16 +11,15 @@ int main(int argc, char *argv[])
         return 1;
     }
     unsigned char buffer[10000];
-    int i = 0;
     while (fread(buffer, 512, 1, file) == 1)
 {
     char jpegs[50];
     fread(buffer, 512, 1, file);
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        for (int i = 0; i <= 50; i++)
     {
         sprintf(jpegs, "%03i.jpg\n", i);
         printf("%c", jpegs[i]);
-        i++;
     }
 }
 }
