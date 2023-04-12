08af77df20191099d09d38ec4fd4c205388b6c5a
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-12 19:49:49 +0000

    Wed, Apr 12, 2023, 9:49 PM +02:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -39,7 +39,7 @@ int convert(string input)
         int result = input[i] - '0';
         input[i] = '\0';
         printf("%i\n", result);
-        total += result * 10 ^ j;
+        total = result * 10 ^ j;
         printf("total is %i\n", result);
         j++;
         convert(input);
