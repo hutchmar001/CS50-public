2de70a26c2ac0cb9d7bc88f66fbfc381f29a8e1b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-04-15 15:39:00 +0000

    Sat, Apr 15, 2023, 5:39 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,13 +12,14 @@ int main(int argc, char *argv[])
     }
     unsigned char buffer[512];
     int i = 0;
+    char jpegs[50];
     while (fread(buffer, 1, 512, file) == 512)
 {
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-        char jpegs[50];
-        sprintf(&jpegs[i], "%03i.jpg", i);
-        FILE *img = fopen(&jpegs[i], "w");
+
+        sprintf(&jpegs[0], "%03i.jpg", i);
+        FILE *img = fopen(&jpegs[0], "w");
         fwrite(&buffer[0], 1, 512, img);
         i++;
         fclose(img);
