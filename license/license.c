fa6e861d1aef880c4868acb80f38a3988dbd743d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-05-23 21:15:56 +0000

    Tue, May 23, 2023, 11:15 PM +02:00

--- license/license.c
+++ license/license.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     char buffer[7];
 
     // Create array to store plate numbers
-    char *plates = (char *)malloc(7 * sizeof(char));
+    char plates[7];
 
     FILE *infile = fopen(argv[1], "r");
 
