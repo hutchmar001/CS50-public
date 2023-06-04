60560dfb6a4e8dc1aafdb45aa26142158ef74885
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-04 17:18:09 +0000

    Sun, Jun 4, 2023, 7:18 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -72,8 +72,8 @@ bool load(const char *dictionary)
         strcpy(new_node->word, buffer);
         int index = hash(new_node->word);
         printf("%d\n", index);
-        new_node->next = table[N];
-        table[N] = new_node;
+        new_node->next = table[index];
+        table[index] = new_node;
 
         count++;
     }
