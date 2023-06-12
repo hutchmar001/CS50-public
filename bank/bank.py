4af680146f5731241fde351d1821a329d22f6e0d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-12 09:36:37 +0000

    Mon, Jun 12, 2023, 12:36 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -6,6 +6,10 @@ def check(grtg):
         {
             print("$0")
         }
+    if (greeting == " hello "):
+        {
+            print("$0")
+        }
     elif (greeting[:5] == "hello" and len(greeting) > 5):
         {
             print("$0")
