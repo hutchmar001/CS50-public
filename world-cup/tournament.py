4f7c61c38a98ac652574b3c4bcaabe18ff67d3ad
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 10:25:52 +0000

    Wed, Jun 14, 2023, 1:25 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -63,7 +63,7 @@ def simulate_tournament(teams):
     # TODO
     while(len(teams) > 1):
         simulate_round(teams)
-    print(teams[0])
+        print(teams)
     return teams[0]
 
 if __name__ == "__main__":
