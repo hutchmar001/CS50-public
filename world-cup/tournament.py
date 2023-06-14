7b27139e0cd004c29432c83aef7db4e56ca868a5
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 19:14:22 +0000

    Wed, Jun 14, 2023, 10:14 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -37,7 +37,7 @@ def main():
                 for y in x:
                     if b in x.keys():
                         x[y] += 1
-                        break
+                        continue
         dict[b] = 0
         counts.append(dict)
         tms.append(b)
