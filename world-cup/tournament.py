67b3985acfbca211036b4c16797cfd022efd65dc
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 09:44:05 +0000

    Wed, Jun 14, 2023, 12:44 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -59,7 +59,7 @@ def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
     while(len(teams) > 1):
-        
+        simulate_round(teams)
 
 if __name__ == "__main__":
     main()
