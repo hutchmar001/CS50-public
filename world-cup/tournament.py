5c69af39b67a22a668649b858dbf1ca2fe284b5f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 14:16:12 +0000

    Wed, Jun 14, 2023, 5:16 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -44,6 +44,15 @@ def main():
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
+def find
+
+
+
+def find(lst, key, value):
+    for i, dic in enumerate(lst):
+        if dic[key] == value:
+            return i
+    return -1
 
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
