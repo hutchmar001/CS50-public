0c10e7c077a3746a5c0c79cb5f6d73a99a2f2162
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 09:56:19 +0000

    Wed, Jun 14, 2023, 12:56 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -14,7 +14,7 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
-    teams = []              # TODO: Read teams into memory from file
+    teams = []
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
@@ -22,6 +22,7 @@ def main():
             r = int(row["rating"])
             teams.append(t)
             teams.append(r)
+    simulate_tournament(teams)
 
 
     counts = {}
