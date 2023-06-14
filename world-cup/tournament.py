ab2457fac40422bdfbe43832418a99c6703ec853
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-14 18:13:25 +0000

    Wed, Jun 14, 2023, 9:13 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -33,7 +33,7 @@ def main():
         dict = {}
         x = simulate_tournament(teams)
         if x in tms:
-            counts[x] += 1
+            counts[0][1] += 1
             continue
         dict["team"] = x
         dict["count"] = 0
