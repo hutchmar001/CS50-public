bd285ca7bd4089673549dcbc5c61df4891d39ae5
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-16 09:11:19 +0000

    Fri, Jun 16, 2023, 12:11 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,10 @@
 # TODO
 import cs50
 
-height = input("Please enter an integer.\n")
+height = int(input("Please enter an integer.\n"))
 
 def main():
-    {
-        while height < 1:
-            print(input("Please enter an integer.\n"))
-    }
+
+    while height not in range(1, 9):
+        print(input("Please enter an integer.\n"))
 
