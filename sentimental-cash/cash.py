34fc6fc71fb6a61a4ba0da77da788b4d51269731
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-16 10:28:11 +0000

    Fri, Jun 16, 2023, 1:28 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,12 +2,16 @@
 import cs50
 
 def get_cents():
-
+    global cents
+    try:
+        cents = int(input("Please enter a postive amount.\n"))
+    except ValueError:
+        get_cents()
+    if height not in range(1, 9):
+        get_cents()
 def calculate_quarters(cents):
 def calculate_dimes(cents):
 def calculate_nickels(cents):
 def calculate_pennies(cents):
 
 def main():
-    int cents = get_cents()
-    
