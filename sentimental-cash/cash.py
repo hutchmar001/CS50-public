b7f020d13219780d04aabbf43ee28d4de3b229dd
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-16 10:25:49 +0000

    Fri, Jun 16, 2023, 1:25 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,8 +2,12 @@
 import cs50
 
 def get_cents():
-    
+
 def calculate_quarters(cents):
 def calculate_dimes(cents):
 def calculate_nickels(cents):
 def calculate_pennies(cents):
+
+def main():
+    int cents = get_cents()
+    
