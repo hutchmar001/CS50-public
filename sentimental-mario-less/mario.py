fbe34afb0a24aff991ecbac6f7dcaa5a4b0177f0
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-16 09:04:59 +0000

    Fri, Jun 16, 2023, 12:04 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,6 +5,6 @@ height = input("Please enter an integer.\n")
 
 def main():
     {
-        while (height not in range(1, 8)):
+        while height not in range(1, 8):
             print(input("Please enter an integer.\n"))
     }
