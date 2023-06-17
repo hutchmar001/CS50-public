d39f43aff4fbedf260e5a492051d59633d7cdabb
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-17 20:41:18 +0000

    Sat, Jun 17, 2023, 11:41 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -23,6 +23,7 @@ def main():
     for index in range(len(databases)): ## For each dictionary
         for k, v in databases[index].items(): ## For each item
             dict_parse = {k: int(v) if v.isnumeric() else v for k, v in databases[index].items()}
+            ## Change all numbers from strings to ints
             databases2.append(dict_parse)
             break
 
