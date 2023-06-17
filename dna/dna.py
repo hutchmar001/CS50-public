a64fb4dca9f9659d6efcdf93b2f87a36ddc36932
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-17 10:53:08 +0000

    Sat, Jun 17, 2023, 1:53 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,7 +12,7 @@ def main():
 
     # TODO: Read database file into a variable
 
-    databases = []
+    databases = defaultdict(list)
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
