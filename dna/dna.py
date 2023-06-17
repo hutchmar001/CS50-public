c0634c890bbb94cf32afde3acbf8941732e39055
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-17 09:32:12 +0000

    Sat, Jun 17, 2023, 12:32 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -30,7 +30,7 @@ def main():
         sequence = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    print(databases.keys())
+    print(databases[0].keys())
     for i in range(0, len(databases)):
         longest_match(sequence, "GCT")
     # TODO: Check database for matching profiles
