d9f219fa01f5bf78c87a4a9e296ce1338204a613
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 19:23:19 +0000

    Sun, Jun 25, 2023, 10:23 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -1,12 +1,10 @@
 from cs50 import SQL
 
-db = SQL("sqlite:///roster.db")
+db = sqlite3.connect('roster.db')
 
 cursor = db.cursor()
 rows ="""CREATE TABLE STUDENT(id INTEGER, student_name VARCHAR(255));"""
 cursor.execute(rows)
-
-
 
 for row in rows:
     print(row)
