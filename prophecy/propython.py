a27260d1b3aeeeb683431800d68460f2e1f6a6ce
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 19:38:32 +0000

    Sun, Jun 25, 2023, 10:38 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -6,5 +6,5 @@ cursor = db.cursor()
 reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
-    curs.execute("INSERT INTO PCFC (type, term, definition) VALUES (?, ?, ?);", to_db)
+    cursor.execute("INSERT INTO PCFC (type, term, definition) VALUES (?, ?, ?);", to_db)
 db.commit()
