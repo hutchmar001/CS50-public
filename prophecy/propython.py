00f9c286c93348d432e48b364f817849b2e7c721
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 20:11:08 +0000

    Sun, Jun 25, 2023, 11:11 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -6,5 +6,5 @@ cursor = db.cursor()
 reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     to_db = [(row[0], "utf8"), (row[1], "utf8"), (row[2], "utf8")]
-    cursor.execute("INSERT INTO  (type, term, definition) VALUES (?, ?, ?);", to_db)
+    cursor.execute("INSERT INTO STUDENTS(id, student_name) VALUES (?, ?);", to_db)
 db.commit()
