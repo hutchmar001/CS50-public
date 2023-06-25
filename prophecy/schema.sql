5079df0f9befa67789a58ee373416a93671be254
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 00:00:21 +0000

    Sun, Jun 25, 2023, 3:00 AM +03:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,28 +1,28 @@
-CREATE TABLE "students" (
-    "id" INTEGER,
-    "student_name" TEXT,
-    "house" TEXT,
-    "head" TEXT,
+CREATE TABLE students (
+    id INTEGER,
+    student_name TEXT,
+    house TEXT,
+    head TEXT,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE "student" (
-    "id" INTEGER,
-    "student_name" TEXT,
+CREATE TABLE student (
+    id INTEGER,
+    student_name TEXT,
     FOREIGN KEY (id) REFERENCES students(id)
     FOREIGN KEY (student_name) REFERENCES students(student_name)
 );
 
-CREATE TABLE "houses" (
-    "house" TEXT,
-    "head" TEXT,
+CREATE TABLE houses (
+    house TEXT,
+    head TEXT,
     FOREIGN KEY (house) REFERENCES students(house)
     FOREIGN KEY (head) REFERENCES students(head)
 );
 
-CREATE TABLE "assignments" (
-    "id" INTEGER,
-    "house" TEXT,
+CREATE TABLE assignments (
+    id INTEGER,
+    house TEXT,
     FOREIGN KEY (id) REFERENCES students(id)
     FOREIGN KEY (house) REFERENCES students(house)
 );
