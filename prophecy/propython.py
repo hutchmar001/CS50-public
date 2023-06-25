5abb18cb291cecf2ecf56fa9e1b4f8a00d96377f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 19:42:48 +0000

    Sun, Jun 25, 2023, 10:42 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -1,8 +1,8 @@
 import csv, sqlite3
 
 db = sqlite3.connect('roster.db')
-
 cursor = db.cursor()
+
 reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
