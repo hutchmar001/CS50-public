a6d00d7fc5854fdbec922172a5d26c979cee563a
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-25 19:36:06 +0000

    Sun, Jun 25, 2023, 10:36 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -3,6 +3,6 @@ import csv, sqlite3
 db = sqlite3.connect('roster.db')
 
 cursor = db.cursor()
-reader = csv.reader(open('students.csv', 'r'), delimiter='|')
+reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     print(row)
