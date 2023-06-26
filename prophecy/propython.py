41fe7ededecbbb8d61633136c8b36b15cc3770af
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 19:51:44 +0000

    Mon, Jun 26, 2023, 10:51 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -18,12 +18,12 @@ get_input()
 
 
 with open('students.csv', "r") as file:
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
     for row in reader:
-        to_db0 = row[0]
-        to_db1 = row[1]
-        to_db2 = row[2]
-        to_db3 = row[3]
+        to_db0 = row['id']
+        to_db1 = row['student_name']
+        to_db2 = row['house']
+        to_db3 = row['head']
         db.execute('INSERT OR IGNORE INTO student(id, student_name) VALUES (?,?);', (to_db0, to_db1))
         db.execute('INSERT OR IGNORE INTO houses(house, head) VALUES (?,?);', (to_db2, to_db3))
         db.execute('INSERT OR IGNORE INTO assignments(id, house) VALUES (?,?);', (to_db0, to_db2))
