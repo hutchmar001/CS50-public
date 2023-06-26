d45764111a50471d7bba2062d0d94ccd24f70d5b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 17:24:37 +0000

    Mon, Jun 26, 2023, 8:24 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -7,7 +7,11 @@ with open('students.csv') as file:
     for row in reader:
         to_db0 = row['id']
         to_db1 = row['student_name']
+        to_db2 = row['house']
+        to_db3 = row['head']
         db.execute('INSERT OR IGNORE INTO STUDENT(id, student_name) VALUES (?,?)', (to_db0, to_db1))
+        db.execute('INSERT OR IGNORE INTO HOUSES(house, head) VALUES (?,?)', (to_db2, to_db3))
+        db.execute('INSERT OR IGNORE INTO ASSIGNMENTS(id, house) VALUES (?,?)', (to_db0, to_db2))
     db.commit()
     db.execute('SELECT * FROM STUDENT')
 
