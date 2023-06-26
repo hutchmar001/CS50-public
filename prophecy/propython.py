e804c7a8a8a942c136bd5bfb346b5f2ba77f2494
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:28:43 +0000

    Mon, Jun 26, 2023, 3:28 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -6,5 +6,4 @@ reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     to_db = [row[0], row[1]]
     db.execute('INSERT INTO STUDENT VALUES (?,?)', to_db)
-            ## ('insert into stocks values (?,?,?,?,?)', t)
 db.commit()
