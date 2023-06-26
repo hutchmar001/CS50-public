b6ff119d6ea9f72da40170d95be462e4be0d7909
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:16:30 +0000

    Mon, Jun 26, 2023, 3:16 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -1,10 +1,8 @@
 import csv, sqlite3
 
 db = sqlite3.connect('roster.db')
-cursor = db.cursor()
 
 reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
-    to_db = row[0]
-    cursor.execute("INSERT INTO STUDENT(id) VALUES (?);", to_db)
+    db.execute(INSERT INTO STUDENT(id) row[0])
 db.commit()
