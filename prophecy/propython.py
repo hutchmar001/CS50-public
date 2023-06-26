4087f09faeee07eb6a3fe16fda642e893f01887e
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:43:52 +0000

    Mon, Jun 26, 2023, 3:43 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -6,5 +6,5 @@ reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
     to_db0 = row[0]
     to_db1 = row[1]
-    db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', to_db0, to_db1)
+    db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', (to_db0, to_db1))
 db.commit()
