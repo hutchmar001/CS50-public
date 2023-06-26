82df4b1fcbdb728e22337c38aad57300f39c002c
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:57:31 +0000

    Mon, Jun 26, 2023, 3:57 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -2,9 +2,11 @@ import csv, sqlite3
 
 db = sqlite3.connect('roster.db')
 
-reader = csv.DictReader(open('students.csv', 'r'))
-for row in reader:
-    to_db0 = row[0]
-    to_db1 = row[1]
-    db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', (to_db0, to_db1))
+with open('students.csv') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        to_db0 = row[0]
+        to_db1 = row[1]
+        db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', (to_db0, to_db1))
 db.commit()
+
