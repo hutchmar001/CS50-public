cf78aaa7fb23b1659cda8fa9afe4a7b6c9223fd6
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:26:59 +0000

    Mon, Jun 26, 2023, 3:26 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -4,6 +4,7 @@ db = sqlite3.connect('roster.db')
 
 reader = csv.reader(open('students.csv', 'r'))
 for row in reader:
-    db.execute('INSERT INTO STUDENT VALUES (?,?)', row)
+    to_db = [row[0], row[1]]
+    db.execute('INSERT INTO STUDENT VALUES (?,?)', to_db)
             ## ('insert into stocks values (?,?,?,?,?)', t)
 db.commit()
