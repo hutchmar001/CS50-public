61718aa2bf3411c6ec73e5f6a88d85690740889b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:39:16 +0000

    Mon, Jun 26, 2023, 3:39 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -7,5 +7,4 @@ for row in reader:
     to_db0 = row[0]
     to_db1 = row[1]
     db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', to_db0, to_db1)
-            ## ('insert into stocks values (?,?,?,?,?)', t)
 db.commit()
