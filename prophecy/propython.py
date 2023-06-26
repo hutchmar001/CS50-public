8f06cb146b4facdf271b778789201944595a512f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 12:58:04 +0000

    Mon, Jun 26, 2023, 3:58 PM +03:00

--- prophecy/propython.py
+++ prophecy/propython.py
@@ -8,5 +8,5 @@ with open('students.csv') as file:
         to_db0 = row[0]
         to_db1 = row[1]
         db.execute('INSERT INTO STUDENT(id, student_name) VALUES (?,?)', (to_db0, to_db1))
-db.commit()
+    db.commit()
 
