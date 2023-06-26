c85ad2b3287284de793c2b829abede531bc0567c
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-26 18:35:36 +0000

    Mon, Jun 26, 2023, 9:35 PM +03:00

---
+++ prophecy/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "student" (
+    "student_id" INTEGER,
+    "student_name" TEXT,
+    PRIMARY KEY(student_id)
+);
+
+CREATE TABLE "houses" (
+    "house" TEXT,
+    "head" TEXT,
+    PRIMARY KEY(house)
+);
+
+CREATE TABLE "assignments" (
+    "id" INTEGER,
+    "assigned_house" TEXT,
+    FOREIGN KEY (id) REFERENCES student(student_id),
+    FOREIGN KEY (assigned_house) REFERENCES houses(house)
+);
