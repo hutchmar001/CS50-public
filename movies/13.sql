edb0f50e93b41872c87956ba0a5817c396ff8905
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 15:36:22 +0000

    Wed, Jun 28, 2023, 6:36 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,11 @@
 SELECT name FROM stars
-WHERE 
+INNER JOIN people ON people.id = stars.person_id
+INNER JOIN movies ON movies.id = stars.movie_id
+WHERE title NOT IN
 (
 SELECT title FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
 WHERE "Kevin Bacon" IN (name) AND birth = "1958"
 )
+LIMIT 5;
