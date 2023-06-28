bc2e74075e38a87bc2f2217212124996a12b7a30
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 14:05:40 +0000

    Wed, Jun 28, 2023, 5:05 PM +03:00

---
+++ movies/11.sql
@@ -0,0 +1,7 @@
+SELECT name FROM stars
+INNER JOIN people ON people.id = stars.person_id
+INNER JOIN movies ON movies.id = stars.movie_id
+INNER JOIN movies ON movies.id = stars.movie_id
+WHERE name = "Chadwick Boseman"
+ORDER BY
+LIMIT 5;
