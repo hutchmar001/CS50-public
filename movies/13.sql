f1d5498334153171b92aa6610c177fcb7158a29f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 15:37:38 +0000

    Wed, Jun 28, 2023, 6:37 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,7 @@
 SELECT name FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
-WHERE title NOT IN
+WHERE title IN
 (
 SELECT title FROM stars
 INNER JOIN people ON people.id = stars.person_id
