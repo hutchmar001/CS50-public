671e4f9fbe930898852aa4258a460b584e650db1
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 14:38:39 +0000

    Wed, Jun 28, 2023, 5:38 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,4 @@
 SELECT title FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
-WHERE EXISTS (SELECT  FROM "Helena Bonham Carter" IN (name))
-
-
-IN (name) AND "Johnny Depp" IN (name);
+WHERE EXISTS (SELECT title FROM stars WHERE "Helena Bonham Carter" IN (name));
