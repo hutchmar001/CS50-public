b68bbe76fc0b7685c5d99095b2889f91b5afb90d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 15:31:14 +0000

    Wed, Jun 28, 2023, 6:31 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,4 +4,4 @@ SELECT * FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
 WHERE "Kevin Bacon" IN (name) AND birth = "1958"
-);
+) AS SUBQUERY;
