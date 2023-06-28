d99e6b3ebd6e19c83b1c07f14e1f0dd55938806b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 15:28:09 +0000

    Wed, Jun 28, 2023, 6:28 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,7 @@
+SELECT * FROM
+(
 SELECT * FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
 WHERE "Kevin Bacon" IN (name) AND birth = "1958"
-INTERSECT
-SELECT * FROM stars
-INNER JOIN people ON people.id = stars.person_id
-INNER JOIN movies ON movies.id = stars.movie_id;
+);
