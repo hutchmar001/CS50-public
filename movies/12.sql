08c024eadf0355eb8e506367f02a2cfacf40cc22
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 14:48:36 +0000

    Wed, Jun 28, 2023, 5:48 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,9 @@
 SELECT title FROM stars
 INNER JOIN people ON people.id = stars.person_id
 INNER JOIN movies ON movies.id = stars.movie_id
-WHERE "Helena Bonham Carter" IN (name) INTERSECT WHERE "Johnny Depp" IN (name);
+WHERE "Helena Bonham Carter" IN (name)
+INTERSECT
+SELECT title FROM stars
+INNER JOIN people ON people.id = stars.person_id
+INNER JOIN movies ON movies.id = stars.movie_id
+WHERE "Johnny Depp" IN (name);
