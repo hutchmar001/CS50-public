5d3c7f1908c05d4dbcf413932647433d7951cf87
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-06-28 13:53:35 +0000

    Wed, Jun 28, 2023, 4:53 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,5 @@
-SELECT person_id FROM movies
+SELECT name FROM movies
 INNER JOIN directors ON directors.movie_id = movies.id
 INNER JOIN ratings ON ratings.movie_id = movies.id
+INNER JOIN people ON people.id = movies.person_id
 WHERE rating >= "9.0";
