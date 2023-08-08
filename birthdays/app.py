c722a35402b50f72f24cbdb82d16afe56afc9496
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-08 21:19:01 +0000

    Wed, Aug 9, 2023, 12:19 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        result = db.execute("SELECT *")
+        result = db.execute("SELECT * FROM birthdays")
         print result
         return render_template("index.html")
 
