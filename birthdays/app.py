469f01a46ac76719bda9f45cce0a1b6653f1dd92
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-08 23:27:29 +0000

    Wed, Aug 9, 2023, 2:27 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,11 +27,11 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        name = request.form.get("name")
-        month = request.form.get("month")
-        day = request.form.get("day")
+        n = request.form.get("name")
+        m = request.form.get("month")
+        d = request.form.get("day")
         print (name)
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", n, m, d)
         return redirect("/")
 
     else:
