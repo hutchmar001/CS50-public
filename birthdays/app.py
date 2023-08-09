d7052917829b5b79dd5e737cd0e4d733f57ef4a8
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 22:24:35 +0000

    Thu, Aug 10, 2023, 1:24 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -35,10 +35,9 @@ def index():
             notification.notify(
                 title = 'testing',
                 message = 'message',
-                app_icon = "image.ico",
                 timeout = 10,
             )
-            return redirect("/")
+
             # Prints name of value into terminal
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", n, m, d)
         return redirect("/")
