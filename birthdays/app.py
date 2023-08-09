3343a09c8ed5d13ced63baeb14b52389979c69d6
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 20:18:53 +0000

    Wed, Aug 9, 2023, 11:18 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -1,13 +1,5 @@
 import os, sys
-import tkinter
 
-root = tkinter.Tk()
-root.title("tkinter example")
-
-label = tkinter.Label(root, text="Hello World!")
-label.pack()
-
-root.mainloop()
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
