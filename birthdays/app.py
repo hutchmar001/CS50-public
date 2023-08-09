08acb858d707c472e5fe6050f6f1c1fe14a25314
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 00:02:27 +0000

    Wed, Aug 9, 2023, 3:02 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         n = request.form.get("name")
         m = request.form.get("month")
         d = request.form.get("day")
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?);", n, m, d)
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", n, m, d,)
         return redirect("/")
 
     else:
