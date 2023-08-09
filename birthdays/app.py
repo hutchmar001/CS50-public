207297666d191252522bef8ab7c3a2d01b6c91f4
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 20:01:13 +0000

    Wed, Aug 9, 2023, 11:01 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -1,15 +1,8 @@
-import os, sys, tkinter
+import os, sys
 
-from tkinter import *
-from tkinter import messagebox
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
-root = Tk()
-root.geometry("300x200")
-w = Label(root, text ='GeeksForGeeks', font = "50")
-w.pack()
-messagebox.showinfo("showinfo", "Information")
 
 # Configure application
 app = Flask(__name__)
