bbc9b7b169b3614ec8f69e0f397bd3f9691ea4d9
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 23:38:41 +0000

    Thu, Aug 10, 2023, 2:38 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -4,7 +4,7 @@ import js2py
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
-code_2 = "function f(x) {return x+x;}"
+code_2 = "function f(x) {return 2;}"
 
 # Configure application
 app = Flask(__name__)
