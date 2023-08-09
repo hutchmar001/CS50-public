fbf194b87c920742b4bcfe1b26f40a53c6ae669c
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 22:01:50 +0000

    Thu, Aug 10, 2023, 1:01 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -6,7 +6,7 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 
 if __name__== "__main__":
 
-        plyerrr.notification.notify(
+        plyer.notification.notify(
             title = "HEADING HERE",
             message=" DESCRIPTION HERE" ,
 
