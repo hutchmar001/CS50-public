75d4bfd8a785c84280cb7c4811a77ba3604b3a55
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-09 23:42:27 +0000

    Thu, Aug 10, 2023, 2:42 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -4,7 +4,7 @@ import js2py
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
-code_2 = "function f(x) {return 2;}"
+code_2 = "alert("Hello! I am an alert box!!");"
 
 # Configure application
 app = Flask(__name__)
