279b4b88e0b8a658e83ee787f7e9215736491164
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-28 20:38:37 +0000

    Mon, Aug 28, 2023, 11:38 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -121,7 +121,7 @@ def register():
         if p1 != p2:
             return apology("passwords must match", 403)
     else:
-        db.execute("INSERT INTO users )
+        db.execute("INSERT INTO users VALUES () )
         return render_template("register.html")
 
 
