db2802b5a48becc2e239c951b41e21823cf0699f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-28 21:05:16 +0000

    Tue, Aug 29, 2023, 12:05 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -121,6 +121,7 @@ def register():
         if p1 != p2:
             return apology("passwords must match", 403)
         u = request.form.get("username")
+        repeat = db.execute("SELECT * FROM users WHERE username == ?", u)
         
         h = generate_password_hash(password=p1)
         db.execute("INSERT INTO users (username, hash) VALUES (?,?)", u, h)
