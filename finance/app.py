7dee8ea24b4f49a84124675ff8c57aad6df40db3
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-29 09:20:42 +0000

    Tue, Aug 29, 2023, 12:20 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -130,7 +130,9 @@ def register():
 
         h = generate_password_hash(password=p1)
         db.execute("INSERT INTO users (username, hash) VALUES (?,?);", u, h)
+        session["user_id", 200] = u
         return render_template("register.html")
+
     else:
         return render_template("register.html")
 
