8333f9b2598c092750828761d519bf577945c840
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-30 09:17:16 +0000

    Wed, Aug 30, 2023, 12:17 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -38,8 +38,7 @@ def index():
     u = user[0]["username"]
     stocks = db.execute("SELECT stock FROM purchases WHERE username = ?;", u)
     st = stocks[0]["stock"]
-    shares = db.execute("SELECT shares FROM purchases GROUP BY username HAVING username = ?;", u)
-    sh = shares[0]["shares"]
+    sh = db.execute("SELECT SUM(shares) FROM purchases;")
     return render_template("home.html", name = u, stock = st, shares = sh)
 
 
