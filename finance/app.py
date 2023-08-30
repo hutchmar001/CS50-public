87ed755d0e79c6b918da03468db7eca5a34d5b7d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-30 18:16:55 +0000

    Wed, Aug 30, 2023, 9:16 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -59,7 +59,7 @@ def index():
             gttr = ([getter(item) for item in a_balance])
             ab = gttr[0]
         db.execute("INSERT INTO home VALUES (?, ?, ?, ?, ?);", u, st, sh, sum, ab)
-        rows = c.execute('SELECT * FROM home')
+        rows = c.execute('SELECT * FROM home;').fetchall()
         for row in rows:
             print(row)
         return render_template('home.html', rows = c.fetchall())
