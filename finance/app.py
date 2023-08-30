0ae98b90743ec5a15915635e5f0d3a94e1a2b98e
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-08-30 11:16:23 +0000

    Wed, Aug 30, 2023, 2:16 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -47,8 +47,8 @@ def index():
             shares = db.execute("SELECT SUM(shares) FROM purchases WHERE stock = ? AND username = ?;", st, u)
             print(shares)
             getter = itemgetter('SUM(shares)')
-            print ([getter(item) for item in stocks])
-            ## sh = (i.get('SUM(shares)'))
+            print ([getter(item) for item in shares])
+            sh = 
             total = db.execute("SELECT SUM(price) FROM purchases WHERE stock = ? AND username = ?;", st, u)
             sum = total[i]["SUM(price)"]
             a_balance = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])
