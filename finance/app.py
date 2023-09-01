894d26c96131de40c5c185e454539e6ca5b2b98b
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-01 06:35:12 +0000

    Fri, Sep 1, 2023, 9:35 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -258,6 +258,8 @@ def sell():
 
         a_balance = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])
         a_b = a_balance[0]["cash"]
+
+        
         if a_b < total_price:
             return apology("You do not have enough money to complete this transaction", 400)
 
