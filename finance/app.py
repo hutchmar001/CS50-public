073f143b2a6a6ac9ee46a1f778ee69df5654a938
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-01 09:59:18 +0000

    Fri, Sep 1, 2023, 12:59 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -278,5 +278,6 @@ def sell():
 
     else:
         stock_select = db.execute("SELECT stock FROM home;")
+        stock_list = []
         print(stock_select)
         return render_template("sell.html")
