8568580d553e6001424b88d964902baed0b1488e
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-01 08:47:48 +0000

    Fri, Sep 1, 2023, 11:47 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -265,9 +265,9 @@ def sell():
         shares = st[0]["shares"]
         if shares < total_shares:
             return apology("You do not have enough shares to sell", 400)
-        shares_update = shares - total_shares
+        shares_update = int(shares - total_shares)
         print(shares_update)
-        db.execute("UPDATE home SET shares = ? WHERE username = ? AND stock = ?;", shares_update, u, stock)
+        db.execute("UPDATE home SET shares = ? WHERE stock = ?;", shares_update, stock)
 
         a_b = a_b + total_price
         db.execute("UPDATE users SET cash = ? WHERE username = ?;", a_b, u)
