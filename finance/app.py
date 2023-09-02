35a62ee8390b91558d673a9b9a5aa5e0c2df0852
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-02 20:37:30 +0000

    Sat, Sep 2, 2023, 11:37 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -46,8 +46,6 @@ def index():
     if stocks:
         for i in stocks:
             st = (i.get('stock'))
-            stock = lookup(st)
-            print(stock)
 
             shares = db.execute("SELECT SUM(shares) FROM purchases WHERE stock = ? AND username = ?;", st, u)
             getter = itemgetter('SUM(shares)')
