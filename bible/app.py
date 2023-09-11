e5b02cf4b6de736eb273ed800cb27bec511fb21e
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-11 22:56:34 +0000

    Tue, Sep 12, 2023, 1:56 AM +03:00

--- bible/app.py
+++ bible/app.py
@@ -45,7 +45,7 @@ def search():
 
         search = request.form.get("search")
         search = db.execute("SELECT * FROM verses WHERE id = ?;", search)
-        s = search[1]["book"]
+        s = search[0]["id"]
         print(search)
         print(s)
 
