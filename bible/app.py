5f09fe24ffb3e5474d508350e99088e6559789c3
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-12 21:35:27 +0000

    Wed, Sep 13, 2023, 12:35 AM +03:00

--- bible/app.py
+++ bible/app.py
@@ -49,7 +49,7 @@ def search():
                 chapter = i["chapter"]
                 verse = i["verse"]
                 text = i["text"]
-                db.execute("INSERT INTO home VALUES (?, ?, ?, ?, ?);", no, book, chapter, verse, text)
+                db.execute("INSERT INTO results VALUES (?, ?, ?, ?, ?);", no, book, chapter, verse, text)
 
         return render_template('home.html', lst=lst)
 
