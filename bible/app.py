f7e20108f6af492f5c79c5c3fac37cb1cd8d3747
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-18 12:29:02 +0000

    Mon, Sep 18, 2023, 3:29 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -108,6 +108,6 @@ def search():
 
         db1.execute("DELETE FROM results;")
         db2.execute("DELETE FROM results;")
-        return render_template('home.html', lst=lst, lst2=lst2)
+        return render_template('home.html', lst=lst, lst2=lst2, lst3=lst3)
 
     return render_template("search.html")
