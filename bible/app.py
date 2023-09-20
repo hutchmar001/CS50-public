cc81ba93ebf102479893734350533a48f01d6a0d
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-20 22:09:44 +0000

    Thu, Sep 21, 2023, 1:09 AM +03:00

--- bible/app.py
+++ bible/app.py
@@ -55,12 +55,9 @@ def search():
         search = request.form.get("search")
 
         # Bible
-         s = db1.execute("SELECT * FROM verses WHERE text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? \
+         s = db1.execute("SELECT * FROM verses WHERE text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? \
                     OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? OR text LIKE ? \
-                    OR text LIKE ? OR text LIKE ?", ('% ' + search + ' %'), ('%' + search + ',%'), ('%"' + search + '%'), \
-                    ('%' + search + '?%'), ('%' + search + '!%'), ('%\'' + search + '%'), ('%' + search + '.%'), \
-                    ('%.' + search + '%'), ('%' + search + ']%'), ('%' + search + ';%'), ('%,' + search + '%'), \
-                    ('%' + search + '-%'), ('%-' + search + '%'), ('%(' + search + ')%'))
+                    OR text LIKE ?", ('% ' + search + ' %'), ('%' + search + ',%'), ('%"' + search + '%'), ('%' + search + '?%'), ('%' + search + '!%'), ('%\'' + search + '%'), ('%' + search + '.%'), \('%.' + search + '%'), ('%' + search + ']%'), ('%' + search + ';%'), ('%,' + search + '%'), \('%' + search + '-%'), ('%-' + search + '%'), ('%(' + search + ')%'))
         # Notice the extra space before/after the %, this ensures that only the complete word is counted as a result
         if s:
             num = 1
