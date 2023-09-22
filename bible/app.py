4f41af78dcc3de7a48d9639214a3b4d48afe757f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-22 21:01:49 +0000

    Sat, Sep 23, 2023, 12:01 AM +03:00

--- bible/app.py
+++ bible/app.py
@@ -241,8 +241,6 @@ def verse():
             lst = db1.execute("SELECT * FROM verses WHERE book_name == ?", bible_name)
             return render_template('home.html', lst=lst, display1="visible", display2="none", display3="none", display_title="none", display_select="none", display_img="none")
 
-
-
         else:
             return render_template("verse.html")
 
