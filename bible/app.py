f171f1e95669f288d6fc759c7824f874923348d8
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-23 20:07:52 +0000

    Sat, Sep 23, 2023, 11:07 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, render_template, request
 from flask_session import Session
 
 
