ee2c4ed99495c35766047b050a7156324c7f71ab
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-24 10:12:04 +0000

    Sun, Sep 24, 2023, 1:12 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -97,7 +97,7 @@ def search():
             result = ["Bhagavad Gita"]
             return render_template('home.html', lst3=lst3, display1="none", display2="none", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result)
 
-    flash('Please enter a valid query.')
+    flash('No results')
     return render_template("search.html")
 
 
