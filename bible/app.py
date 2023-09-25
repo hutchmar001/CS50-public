01e3f4eab4681b67f063477d9e1b335f4bcf55ad
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-25 22:03:20 +0000

    Tue, Sep 26, 2023, 1:03 AM +03:00

--- bible/app.py
+++ bible/app.py
@@ -44,7 +44,7 @@ def after_request(response):
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("home.html", display1="none", display2="none", display3="none", display_select="none", display_img="inline", search="")
+    return render_template("home.html", display1="none", display2="none", display3="none", display_select="none", display_img="inline", display_hlight="none" search="")
     # set search to "" to avoid JSON error
 
 @app.route("/search", methods=["GET", "POST"])
