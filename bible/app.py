9b8e6a46b62388c43510b8fc8ad819f6f2834720
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-09-26 09:42:20 +0000

    Tue, Sep 26, 2023, 12:42 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -58,7 +58,7 @@ def search():
         if search == " ":
             flash('Please enter a valid query.')
             return redirect("/search")
-        search_lower = search.lower()
+
         search_upper = search.title()
 
         # Bible
@@ -85,25 +85,25 @@ def search():
 
         if lst and lst2 and lst3:
             result = ["Bible", "Quran", "Bhagavad Gita"]
-            return render_template('home.html', lst=lst, lst2=lst2, lst3=lst3, display1="visible", display2="visible", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst=lst, lst2=lst2, lst3=lst3, display1="visible", display2="visible", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst and lst2:
             result = ["Bible", "Quran"]
-            return render_template('home.html', lst=lst, lst2=lst2, display1="visible", display2="visible", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst=lst, lst2=lst2, display1="visible", display2="visible", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst and lst3:
             result = ["Bible", "Bhagavad Gita"]
-            return render_template('home.html', lst=lst, lst3=lst3, display1="visible", display2="none", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst=lst, lst3=lst3, display1="visible", display2="none", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst2 and lst3:
             result = ["Quran", "Bhagavad Gita"]
-            return render_template('home.html', lst2=lst2, lst3=lst3, display1="none", display2="visible", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst2=lst2, lst3=lst3, display1="none", display2="visible", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst:
             result = ["Bible"]
-            return render_template('home.html', lst=lst, display1="visible", display2="none", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst=lst, display1="visible", display2="none", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst2:
             result = ["Quran"]
-            return render_template('home.html', lst2=lst2, display1="none", display2="visible", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst2=lst2, display1="none", display2="visible", display3="none", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
         if lst3:
             result = ["Bhagavad Gita"]
-            return render_template('home.html', lst3=lst3, display1="none", display2="none", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_lower = search_lower, search_upper = search_upper)
+            return render_template('home.html', lst3=lst3, display1="none", display2="none", display3="visible", display_title="none", display_select="inline-block", display_img="none", result=result, search_upper = search_upper)
 
         flash('No Results')
         return redirect("/search")
