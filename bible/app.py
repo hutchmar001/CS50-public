7ff88dab479bc7e9ff3379a4ddb0088f37ed9d0f
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-10-08 10:19:59 +0000

    Sun, Oct 8, 2023, 1:19 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -27,7 +27,7 @@ app.run(debug=True)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=0)
 Session(app)
 
 # Configure CS50 Library to use SQLite databases
