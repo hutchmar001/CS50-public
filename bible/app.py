15e3d1f1d236c457ab0b2a5db9de2ba91b29c233
hutchmar001 <118636519+hutchmar001@users.noreply.github.com>
2023-10-08 17:14:39 +0000

    Sun, Oct 8, 2023, 8:14 PM +03:00

--- bible/app.py
+++ bible/app.py
@@ -200,7 +200,6 @@ def Bible():
         rpp = int(float(rpp))
     except Exception as e:
         print("Exception raised: {}".format(e))
-        flash('Please enter a valid query.')
         return render_template('Bible.html', lst=lst, result=result, search_upper=search_upper, search_lower=search_lower, rpp=20, rpp_v="none")
 
     return render_template('Bible.html', lst=lst, result=result, search_upper=search_upper, search_lower=search_lower, rpp=rpp, rpp_v="none")
